3a306f150feaec1e4b893942535c366b1997a042
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-08-29 23:56:02 -0300

    esta função pega somente o nome do arquivo e despresa o caminho

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,25 @@
 package application;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// obtendoCaminhoDoArquivo
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a file path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		// Acessar somente o nome do arquivo e não o caminho completo
+		System.out.println("getName: " + path.getName());
+		
+	
+	
+		sc.close();
 	}
 
 }
